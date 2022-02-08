b01b813acae73a6bc1915d0065e03a691d14461e
Xavier Casafont <XCasafont@globalshares.com>
2022-02-08 19:05:58 +0100

    app binded to port 8080

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var express = require("express");
 var app = express();
 
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
 app.get("/people", (req, res, next) => {
  res.json({"Names":["Tony","Lisa","Michael","Ginger","Melody"]});
 });
 
-app.listen(3000, () => {
- console.log("Server running on port 3000");
-});
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
