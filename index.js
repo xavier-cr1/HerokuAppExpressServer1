caa22942503e546862340387a65d7aad5280a2ff
Xavier Casafont <XCasafont@globalshares.com>
2022-06-22 10:59:38 +0200

    added console log if running local

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 var express = require("express");
 var app = express();
 
+const localPORT = 8080;
+const localHOST = '0.0.0.0';
+
 app.get("/people", (req, res, next) => {
   res.json({ Names: ["Tony", "Lisa", "Michael", "Ginger", "Melodyd,Johnny"] });
 });
 
 app.listen(process.env.PORT || 8080);
-console.log(`Running on http://${process.env.HOST}:${process.env.PORT}`);
+if (process.env.PORT === undefined || variable === null) {
+  console.log(`Running on http://${localHOST}:${localPORT}`);
+}
+else
+{
+  console.log(`Running on http://${process.env.HOST}:${process.env.PORT}`);
+}
+
